38d19cbb8d015243cc2ab05d119c30ec37446388
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-24 19:25:33 -0300

    Update tests for List.jsx

--- src/components/__tests__/List.test.js
+++ src/components/__tests__/List.test.js
@@ -4,43 +4,61 @@ import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import List from '../List';
 
+const peopleDataMock = [
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+];
+
 describe('List', () => {
   it('Renders the amount of birthdays', () => {
-    render(<List />);
+    render(<List peopleData={peopleDataMock} />);
 
     screen.debug();
 
-    expect(screen.getByRole('heading', { name: /5 birthdays/i })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /2 birthdays/i })).toBeInTheDocument();
   });
 
   it("Renders people's data", () => {
-    render(<List />);
+    render(<List peopleData={peopleDataMock} />);
 
     screen.debug();
 
-    const $images = screen.getAllByRole('img');
+    expect(screen.getByRole('img', { name: 'Bertie Yates' })).toBeInTheDocument();
+    expect(screen.getByRole('img', { name: 'Bertie Yates' }).src).toContain('.jpg');
+    expect(screen.getByText('Bertie Yates')).toBeInTheDocument();
+    expect(screen.getByText('29')).toBeInTheDocument();
 
-    expect($images).toHaveLength(5);
-    for (let i = 0; i < $images.length; i += 1) {
-      expect($images[i].src).toContain('.jpg');
-    }
+    expect(screen.getByRole('img', { name: 'Hester Hogan' })).toBeInTheDocument();
+    expect(screen.getByRole('img', { name: 'Hester Hogan' }).src).toContain('.jpg');
+    expect(screen.getByText('Hester Hogan')).toBeInTheDocument();
+    expect(screen.getByText('32')).toBeInTheDocument();
   });
 
   it('Removes all birthdays', () => {
-    render(<List />);
+    render(<List peopleData={peopleDataMock} />);
 
     screen.debug();
 
-    let $images = screen.getAllByRole('img');
+    let $birthdays = screen.getAllByRole('listitem');
 
-    expect($images).toHaveLength(5);
+    expect($birthdays).toHaveLength(2);
 
     userEvent.click(screen.getByRole('button', { name: /Delete all/i }));
 
     screen.debug();
 
-    $images = screen.queryAllByRole('img');
+    $birthdays = screen.queryAllByRole('img');
 
-    expect($images).toHaveLength(0);
+    expect($birthdays).toHaveLength(0);
   });
 });
