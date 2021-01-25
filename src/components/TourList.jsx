b9e6a81182c3ed47d26ba47f4aef0a0d25b7d6ba
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-25 17:04:35 -0300

    Implement data fetch

--- src/components/TourList.jsx
+++ src/components/TourList.jsx
@@ -1,24 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Tour from './Tour';
+import Button from './Button';
 
-const tourData = {
-  id: 1,
-  name: 'Best of Paris in 7 Days Tour',
-  info:
-    "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!",
-  image: 'https://dl.airtable.com/.attachments/a0cd0702c443f31526267f38ea5314a1/2447eb7a/paris.jpg',
-  price: 1.995,
-};
+const apiUrl = 'https://course-api.com/react-tours-project';
 
 const Container = styled.ul`
   list-style: none;
 `;
 
-const TourList = () => (
-  <Container>
-    <Tour {...tourData} />
-  </Container>
-);
+const TourList = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const getTours = async () => {
+    try {
+      setLoading(true);
+      setError(false);
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Something happened!');
+      }
+      const tours = await response.json();
+      setTours(tours);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  const removeTour = (tourId) => {
+    const newTours = tours.filter((tour) => tour.id !== tourId);
+    setTours(newTours);
+  };
+
+  useEffect(() => {
+    getTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <Container>
+        <h2>Loading...</h2>
+      </Container>
+    );
+  }
+
+  if (error) {
+    return (
+      <Container>
+        <h2>Oops! Something went wrong! Please try again.</h2>
+      </Container>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <Container>
+        <Button callback={getTours}>Refresh</Button>
+      </Container>
+    );
+  }
+
+  return (
+    <Container>
+      {tours.length > 0 &&
+        tours.map((tour) => <Tour key={tour.id} {...tour} removeTourCallback={removeTour} />)}
+    </Container>
+  );
+};
 
 export default TourList;
