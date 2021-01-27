5f3f53f06dce14ab812fa4a4b4d96026a43f97a4
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-27 11:29:24 -0300

    Update App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import GlobalStyles from './GlobalStyles';
+import Title from './Title';
+import Card from './Card';
+import peopleData from '../data/people';
 
-const App = () => <h1>Hello World!</h1>;
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 2em;
+`;
+
+const App = () => {
+  const [person, setPerson] = useState(peopleData[0]);
+
+  return (
+    <React.Fragment>
+      <GlobalStyles />
+      <Container>
+        <Title>Our Reviews</Title>
+        <Card {...person} />
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default App;
