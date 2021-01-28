c0e72d44e98ce3f963b3224fd5a5e706a610489f
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-28 10:39:57 -0300

    Create AccordionContainer component

---
+++ src/components/AccordionContainer.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Accordion from './Accordion';
+import questionsData from '../data/questions';
+
+const Container = styled.article`
+  background-color: var(--clr-white-1);
+  padding: 2em;
+  border-radius: 5px;
+  box-shadow: 0.15em 0.15em 0.5em 0.15em #00000025;
+  width: 285px;
+  margin: 2em;
+
+  @media screen and (min-width: 576px) {
+    width: 385px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 80vw;
+    min-width: 485px;
+    display: grid;
+    grid-template-columns: 0.5fr 1fr;
+    grid-template-rows: 1fr;
+    column-gap: 2em;
+
+    & > section > *:first-child {
+      margin-top: 0;
+    }
+  }
+
+  & > section > * {
+    margin-top: 2em;
+  }
+`;
+
+const AccordionContainer = () => {
+  const [questions, setQuestions] = useState(questionsData);
+
+  return (
+    <Container>
+      <h1>Questions and Answers About Login</h1>
+      <section>
+        {questions.map((question) => (
+          <Accordion key={question.id} {...question} />
+        ))}
+      </section>
+    </Container>
+  );
+};
+
+export default AccordionContainer;
