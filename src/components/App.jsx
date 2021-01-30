7f24959e77e9b8013f578b28f914d6932d5bcaeb
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-30 13:28:31 -0300

    Update styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,6 @@ import Tabs from './Tabs';
 const Main = styled.main`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   min-height: 100vh;
   padding: 2em;
