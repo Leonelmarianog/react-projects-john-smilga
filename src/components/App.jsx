f3871b185ba0bbccdf4b931e756b448bf85cca13
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-01-30 11:47:34 -0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import GlobalStyles from './GlobalStyles';
+import Title from './Title';
+import Tabs from './Tabs';
 
-const App = () => <h1>Hello World</h1>;
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 2em;
+
+  & > *:first-child {
+    margin-bottom: 2em;
+  }
+`;
+
+const App = () => (
+  <React.Fragment>
+    <GlobalStyles />
+    <Main>
+      <Title>Experience</Title>
+      <Tabs />
+    </Main>
+  </React.Fragment>
+);
 
 export default App;
