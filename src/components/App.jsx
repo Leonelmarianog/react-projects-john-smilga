7e56ec5dfbdaabe1fa573c7e9e0c7319dc28480b
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-01 16:03:09 -0300

    Update App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import GlobalStyles from './GlobalStyles';
+import Header from './Header';
+import Slider from './Slider';
 
-const App = () => <h1>Hello World!</h1>;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 2em;
+`;
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2em 0;
+`;
+
+const App = () => (
+  <React.Fragment>
+    <GlobalStyles />
+    <Container>
+      <Header title='Reviews' />
+      <Main>
+        <Slider />
+      </Main>
+    </Container>
+  </React.Fragment>
+);
 
 export default App;
