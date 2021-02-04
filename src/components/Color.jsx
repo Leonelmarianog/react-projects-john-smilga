55adadbd48ca8992c97311dadf12e71807794ace
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-04 13:17:29 -0300

    Implement copy color functionality

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const ColorBox = styled.article`
@@ -17,11 +17,39 @@ const Text = styled.p`
   color: var(--clr-white-1);
 `;
 
-const Color = ({ colorHex, colorWeight }) => (
-  <ColorBox colorHex={colorHex}>
-    <Text>{colorWeight}</Text>
-    <Text>{colorHex}</Text>
-  </ColorBox>
-);
+const Helper = styled(Text)`
+  font-size: calc(var(--font-size-base) / 1.7);
+
+  @media screen and (min-width: 768px) {
+    font-size: calc(var(--font-size-base) / 1.2);
+  }
+`;
+
+const Color = ({ colorHex, colorWeight }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleClick = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(colorHex);
+  };
+
+  useEffect(() => {
+    if (copied) {
+      const timerId = setTimeout(() => {
+        setCopied(false);
+      }, 3000);
+
+      return () => clearTimeout(timerId);
+    }
+  }, [copied]);
+
+  return (
+    <ColorBox colorHex={colorHex} onClick={handleClick}>
+      <Text>{colorWeight}</Text>
+      <Text>{colorHex}</Text>
+      <Helper>{copied && 'Copied to clipboard!'}</Helper>
+    </ColorBox>
+  );
+};
 
 export default Color;
