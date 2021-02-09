daf2b261f0c6c69015b9a32997c6642a778cc35b
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-09 20:00:36 -0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import GlobalStyles from './GlobalStyles';
+import Navbar from './Navbar';
 
-const App = () => <h1>Hello World</h1>;
+const Container = styled.div`
+  min-height: 100vh;
+`;
+
+const App = () => (
+  <React.Fragment>
+    <GlobalStyles />
+    <Container>
+      <Navbar />
+    </Container>
+  </React.Fragment>
+);
 
 export default App;
