54a6d00cf4c14a63c6ad01bfaf6e9b7693affa64
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-11 17:23:06 -0300

    Update App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import GlobalStyles from './GlobalStyles';
+import Home from './Home';
+import Sidebar from './Sidebar';
+import { AppProvider } from './GlobalContext';
+import HamburguerButton from './HamburguerButton';
+import Overlay from './Overlay';
 
-const App = () => <h1>Hello World</h1>;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  position: relative;
+`;
+
+const App = () => (
+  <React.Fragment>
+    <AppProvider>
+      <GlobalStyles />
+      <Container>
+        <Overlay />
+        <HamburguerButton />
+        <Home />
+        <Sidebar />
+      </Container>
+    </AppProvider>
+  </React.Fragment>
+);
 
 export default App;
