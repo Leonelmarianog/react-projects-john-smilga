3d689dbf4f42d2ae8e3c8abce8e104a57d5cb2b3
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-13 19:22:48 -0300

    Add global styles

---
+++ src/components/GlobalStyles.jsx
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --font-size-base: 0.65rem;
+    --clr-white-1: #ffffff;
+    --clr-blue-1: #001f38;
+    --clr-blue-2: #005092;
+    --clr-blue-3: #647e94;
+    --clr-blue-4: #8da3b6;
+
+    @media screen and (min-width: 576px) {
+      --font-size-base: 0.85rem;
+    }
+
+    @media screen and (min-width: 576px) {
+      --font-size-base: 1rem;
+    }
+  }
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-size: var(--font-size-base);
+    font-family: 'Lato', 'sans-serif';
+    background: var(--clr-white-1);
+  }
+`;
+
+export default GlobalStyles;
