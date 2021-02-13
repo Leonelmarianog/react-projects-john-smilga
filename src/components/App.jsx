e2d99168e945e0fb8648868339045cb36dd9cd1a
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-13 19:26:36 -0300

    Update App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import GlobalStyles from './GlobalStyles';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Hero from './Hero';
+import Overlay from './Overlay';
+import { AppProvider } from './GlobalContext';
 
-const App = () => <h1>Hello World</h1>;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  position: relative;
+`;
+
+const App = () => (
+  <React.Fragment>
+    <AppProvider>
+      <GlobalStyles />
+      <Container>
+        <Overlay />
+        <Navbar />
+        <Sidebar />
+        <Hero />
+      </Container>
+    </AppProvider>
+  </React.Fragment>
+);
 
 export default App;
