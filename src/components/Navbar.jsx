7963e56b3620212c9d2ec951df27dc5bf457e413
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-15 17:39:43 -0300

    Complete navbar markup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,13 @@ const Container = styled.nav`
   left: 0;
   width: 100%;
   z-index: 1;
+
+  @media screen and (min-width: 992px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1em 10% 0 10%;
+  }
 `;
 
 const NavbarHeader = styled.header`
@@ -22,14 +29,14 @@ const NavbarHeader = styled.header`
   }
 
   @media screen and (min-width: 992px) {
-    padding: 1em 3em 0 3em;
+    padding: 0;
   }
 `;
 
 const HamburguerBtn = styled.button`
   background-color: #ffffff25;
   border: none;
-  font-size: calc(var(--font-size-navbar) * 1);
+  font-size: var(--font-size-navbar);
   padding: 0.5em 1em;
   border-radius: 10px;
   color: var(--clr-white-1);
@@ -57,6 +64,59 @@ const Brand = styled.a`
   }
 `;
 
+const NavbarMenu = styled.ul`
+  list-style: none;
+  display: none;
+
+  & > li {
+    margin-right: 3em;
+  }
+
+  & > li:last-child {
+    margin-right: 0;
+  }
+
+  @media screen and (min-width: 992px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const MenuItem = styled.a`
+  text-decoration: none;
+  color: var(--clr-white-1);
+  font-weight: bold;
+  transition: opacity 0.2s linear;
+
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+const NavbarEnd = styled.div`
+  display: none;
+
+  @media screen and (min-width: 992px) {
+    display: block;
+  }
+`;
+
+const LoginBtn = styled.a`
+  text-decoration: none;
+  background-color: #ffffff25;
+  color: var(--clr-white-1);
+  font-weight: bold;
+  padding: 0.4em 1em 0.5em 1em;
+  border-radius: 20px;
+  transition: background-color 0.2s linear;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #ffffff50;
+  }
+`;
+
 const Navbar = () => {
   const { setIsSidebarMounted } = useGlobalContext();
 
@@ -68,6 +128,25 @@ const Navbar = () => {
           <FaBars />
         </HamburguerBtn>
       </NavbarHeader>
+
+      <NavbarMenu>
+        <li>
+          <MenuItem href='/'>Productos</MenuItem>
+        </li>
+        <li>
+          <MenuItem href='/'>Desarrolladores</MenuItem>
+        </li>
+        <li>
+          <MenuItem href='/'>Empresa</MenuItem>
+        </li>
+        <li>
+          <MenuItem href='/'>Tarifas</MenuItem>
+        </li>
+      </NavbarMenu>
+
+      <NavbarEnd>
+        <LoginBtn>Iniciar sesión</LoginBtn>
+      </NavbarEnd>
     </Container>
   );
 };
