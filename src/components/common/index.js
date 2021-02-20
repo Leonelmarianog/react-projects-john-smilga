a157f40a9f31961643eae982b5a51727b47a0a25
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-20 13:49:00 -0300

    Update common module

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,5 +2,6 @@ import Card from './Card';
 import LinkButton from './LinkButton';
 import LinkIcon from './LinkIcon';
 import List from './List';
+import Link from './Link';
 
-export { Card, LinkButton, LinkIcon, List };
+export { Card, LinkButton, LinkIcon, List, Link };
