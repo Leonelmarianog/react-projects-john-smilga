aa68fa41096583a462a2903a3e316cdf43f6b45d
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-20 13:42:32 -0300

    Remove padding from Card

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const Card = styled.nav`
   background-color: var(--clr-white-1);
-  padding: 1.5em 0 0 0;
   border-radius: 10px;
   box-shadow: 0 2em 2em 0.5em var(--clr-shadow);
   display: flex;
