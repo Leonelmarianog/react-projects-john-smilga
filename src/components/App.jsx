6ea3aa376b7f133c6d6307d87d40bffccf9e967c
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-21 23:56:16 -0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import GlobalStyles from './GlobalStyles';
-import Navbar from './Navbar';
-import Sidebar from './Sidebar';
+import Header from './navigation/Header';
 import Hero from './Hero';
 import Overlay from './Overlay';
-import Submenu from './Submenu';
 import { AppProvider } from './context/GlobalContext';
-import { sidebarData } from '../data/data';
 
 const Container = styled.div`
   display: flex;
@@ -21,10 +18,11 @@ const App = () => (
     <AppProvider>
       <GlobalStyles />
       <Container>
-        <Overlay />
+        {/* <Overlay />
         <Navbar />
         <Submenu />
-        <Sidebar data={sidebarData} />
+        <Sidebar data={sidebarData} /> */}
+        <Header />
         <Hero />
       </Container>
     </AppProvider>
