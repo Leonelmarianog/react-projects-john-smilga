65a8c20a496277818902b343047f07064857d347
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-23 17:41:46 -0300

    Create Bag component

--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -1,39 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FaShoppingBag } from 'react-icons/fa';
+import ItemList from './ItemList';
 
-const Container = styled.a`
-  position: relative;
-  text-decoration: none;
-  color: var(--clr-white-1);
+const Container = styled.article`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  padding: 2em;
 `;
 
-const ItemQuantity = styled.div`
-  background-color: var(--clr-red-3);
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  padding: 0.25em;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: var(--font-size-base);
-  position: absolute;
-  top: -30%;
-  left: 50%;
-`;
-
-const BagIcon = styled(FaShoppingBag)`
-  font-size: calc(var(--font-size-base) * 2);
+const Heading = styled.h1`
+  text-transform: capitalize;
+  margin: 0 auto 2em auto;
 `;
 
 const Bag = () => (
-  <Container href='/'>
-    <ItemQuantity>1</ItemQuantity>
-    <BagIcon />
+  <Container>
+    <Heading>Your Bag</Heading>
+    <ItemList />
   </Container>
 );
 
