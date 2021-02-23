5b5ab14e034596719510f90a9bc569280448a43e
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-23 17:43:55 -0300

    Create ItemCounter component

---
+++ src/components/ItemCounter.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FaPlus, FaMinus } from 'react-icons/fa';
+
+const Container = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  grid-column: 2/-1;
+  grid-row: 1/2;
+`;
+
+const Quantity = styled.span`
+  font-size: calc(var(--font-size-base) * 1.5);
+  margin: 0.25em;
+`;
+
+const ChangeQuantityButton = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  font-size: calc(var(--font-size-base) * 2);
+  color: var(--clr-white-1);
+  transition: color 0.2s linear;
+
+  &:hover {
+    color: var(--clr-red-1);
+  }
+`;
+
+const ItemCounter = () => (
+  <Container>
+    <ChangeQuantityButton>
+      <FaPlus />
+    </ChangeQuantityButton>
+    <Quantity>1</Quantity>
+    <ChangeQuantityButton>
+      <FaMinus />
+    </ChangeQuantityButton>
+  </Container>
+);
+
+export default ItemCounter;
