370e05cafc7f335a477c6d52d190272d4e452c7a
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-23 16:56:48 -0300

    Add Normalize.css to global styles

--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
-const GlobalStyles = createGlobalStyle`
+const GlobalStyles = createGlobalStyle`  
   :root {
     --font-size-base: 1rem;
     --clr-black-1: #000000;
@@ -12,18 +13,19 @@ const GlobalStyles = createGlobalStyle`
     --height-header: 64px;
   }
 
+  ${normalize}
+
   *, *::before, *::after {
     box-sizing: border-box;
+  }
+
+  h1, h2, h3, h4, h5, h6, ul {
     margin: 0;
     padding: 0;
   }
 
-  body, a, button {
-    font-family: 'Noto Sans', sans-serif;
-  }
-
   body {
-    font-size: var(--font-size-base);
+    font-family: 'Noto Sans', 'sans-serif';
     color: var(--clr-white-1);
     background: var(--clr-black-2);
   }
