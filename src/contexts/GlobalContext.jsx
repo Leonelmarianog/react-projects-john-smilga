9bb2fc166b709bca6f8f07746b943eda1c6be9ce
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-24 16:57:37 -0300

    Implement bagReducer

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -1,15 +1,43 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import getResource from '../api/api';
-import { useFetch } from '../hooks';
+import { bagReducer } from '../reducers';
 
 const URL = 'https://course-api.com/react-useReducer-cart-project';
 
 const AppContext = createContext();
 
-const AppProvider = ({ children }) => {
-  const { data, loading, error } = useFetch(getResource, URL);
+const initialState = {
+  loading: true,
+  error: { status: false, message: null },
+  data: null,
+  amount: 0,
+  total: 0,
+};
 
-  return <AppContext.Provider value={{ data, loading, error }}>{children}</AppContext.Provider>;
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(bagReducer, initialState);
+
+  useEffect(() => {
+    const getData = async (fetcher, url) => {
+      dispatch({ type: 'LOAD' });
+
+      try {
+        const data = await fetcher(url);
+
+        if (!data.length > 0) {
+          throw new Error('No items in bag');
+        }
+
+        dispatch({ type: 'SUCCESS', payload: data });
+      } catch (error) {
+        dispatch({ type: 'ERROR', payload: error.message });
+      }
+    };
+
+    getData(getResource, URL);
+  }, []);
+
+  return <AppContext.Provider value={{ state, dispatch }}>{children}</AppContext.Provider>;
 };
 
 export { AppContext, AppProvider };
