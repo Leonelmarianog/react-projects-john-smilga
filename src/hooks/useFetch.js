ba022d9bf0fd3db655691a961836aa9c8d0785eb
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-24 16:49:55 -0300

    Update useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,21 +1,7 @@
 import { useReducer, useEffect } from 'react';
+import { fetchReducer } from '../reducers';
 
 const initialState = { data: null, loading: true, error: { status: false, message: null } };
-
-const fetchReducer = (state, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case 'LOAD':
-      return { ...state, data: null, loading: true, error: { status: false, message: null } };
-    case 'SUCCESS':
-      return { ...state, data: payload, loading: false, error: { status: false, message: null } };
-    case 'ERROR':
-      return { ...state, data: null, loading: false, error: { status: true, message: payload } };
-    default:
-      return state;
-  }
-};
 
 const useFetch = (fetcher, url) => {
   const [state, dispatch] = useReducer(fetchReducer, initialState);
