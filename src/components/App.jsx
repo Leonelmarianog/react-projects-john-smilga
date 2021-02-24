077abb0a56053415fa513651fe6d90a46c460d72
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-02-24 13:00:45 -0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
 import GlobalStyles from './GlobalStyles';
 import Header from './Header';
 import Bag from './Bag';
 
-const Container = styled.div``;
-
 const App = () => (
   <React.Fragment>
     <GlobalStyles />
-    <Container>
+    <div>
       <Header />
       <main>
         <Bag />
       </main>
-    </Container>
+    </div>
   </React.Fragment>
 );
 
